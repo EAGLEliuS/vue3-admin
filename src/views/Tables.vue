06233b70a5276753aad4383256b9e4b45361ced9
HLW\eagle.liu <eagle.liu@hlwtech.com.tw>
2022-05-12 16:32:28 +0800

    add Pagination

--- src/views/Tables.vue
+++ src/views/Tables.vue
@@ -1,26 +1,24 @@
 <script setup>
-import { ref } from 'vue'
-import { mdiMonitorCellphone, mdiAccountMultiple, mdiTableBorder, mdiTableOff } from '@mdi/js'
-import MainSection from '@/components/MainSection.vue'
-import Notification from '@/components/Notification.vue'
-import ClientsTable from '@/components/ClientsTable.vue'
-import CardComponent from '@/components/CardComponent.vue'
-import TitleBar from '@/components/TitleBar.vue'
-import HeroBar from '@/components/HeroBar.vue'
-import BottomOtherPagesSection from '@/components/BottomOtherPagesSection.vue'
-import TitleSubBar from '@/components/TitleSubBar.vue'
+import { ref } from "vue";
+import { mdiMonitorCellphone, mdiAccountMultiple } from "@mdi/js"; //, mdiTableBorder, mdiTableOff
+import MainSection from "@/components/MainSection.vue";
+import Notification from "@/components/Notification.vue";
+import ClientsTable from "@/components/ClientsTable.vue";
+import CardComponent from "@/components/CardComponent.vue";
+// import TitleBar from '@/components/TitleBar.vue'
+import HeroBar from "@/components/HeroBar.vue";
+import BottomOtherPagesSection from "@/components/BottomOtherPagesSection.vue";
+import Pagination from "@/components/Pagination.vue";
+// import TitleSubBar from '@/components/TitleSubBar.vue'
 
-const titleStack = ref(['Admin', 'Tables'])
+const titleStack = ref(["Admin", "Tables"]);
 </script>
 
 <template>
   <title-bar :title-stack="titleStack" />
   <hero-bar>Tables</hero-bar>
   <main-section>
-    <notification
-      color="info"
-      :icon="mdiMonitorCellphone"
-    >
+    <notification color="info" :icon="mdiMonitorCellphone">
       <b>Responsive table.</b> Collapses on mobile
     </notification>
 
@@ -31,9 +29,10 @@ const titleStack = ref(['Admin', 'Tables'])
       has-table
     >
       <clients-table checkable />
+      <pagination></pagination>
     </card-component>
 
-    <title-sub-bar
+    <!-- <title-sub-bar
       :icon="mdiTableBorder"
       title="Wrapped variation"
     />
@@ -64,7 +63,7 @@ const titleStack = ref(['Admin', 'Tables'])
       <b>Empty table.</b> When there's nothing to show
     </notification>
 
-    <card-component empty />
+    <card-component empty /> -->
   </main-section>
 
   <bottom-other-pages-section />
